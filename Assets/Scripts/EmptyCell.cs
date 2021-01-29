6b100556b3a3b6f2611c818eb4590d4f990b786a
peregurin <chinmayvirmani@gmail.com>
2021-01-29 14:40:40 +0530

    basic board setup

---
+++ Assets/Scripts/EmptyCell.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class EmptyCell : MonoBehaviour
+{
+    public Button button;
+    public TMP_Text buttonText;
+
+
+    private void Start()
+    {
+        button.onClick.AddListener(OnButtonClick);
+    }
+
+    private void OnButtonClick()
+    {
+        buttonText.text = "X";
+        button.interactable = false;
+    }
+}
