ca38061d00e05a8a771242dd44fb1c01f9476e81
peregurin <chinmayvirmani@gmail.com>
2021-02-06 18:44:30 +0530

    dictionary functionality achieved
    stored theme names and corresponding colors for 3 UI elements - background, board and gridlines
    using editor script and scriptable objects, change the theme

---
+++ Assets/Editor/ThemeLoaderEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ThemeLoaderScript))]
+public class ThemeLoaderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        if (GUILayout.Button("Set Theme"))
+        {
+            ((ThemeLoaderScript)target).SetTheme();
+        }
+    }
+}
