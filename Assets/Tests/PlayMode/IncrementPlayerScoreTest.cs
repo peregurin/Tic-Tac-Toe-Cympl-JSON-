8541672c74359d5ebd3f9d2a511f66b00ea5e2f2
peregurin <chinmayvirmani@gmail.com>
2021-02-20 19:01:10 +0530

    Unit Testing the playerScoreIncrement fucntion in gameController

---
+++ Assets/Tests/PlayMode/IncrementPlayerScoreTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class IncrementPlayerScoreTest
+    {
+        [Test]
+        public void IncrementPlayerScore_IncreaseScoreByOne()
+        {
+            GameObject gameObject = new GameObject();
+            GameController gameController = gameObject.AddComponent<GameController>();
+
+            gameController.IncrementPlayerScore();
+
+            Assert.AreEqual(1, gameController.PlayerXScore);
+        }
+    }
+}
