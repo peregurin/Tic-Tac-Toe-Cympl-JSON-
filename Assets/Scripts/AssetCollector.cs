49d556aaac181aa47b8cf30818e99084aa996f43
peregurin <chinmayvirmani@gmail.com>
2021-02-20 17:38:25 +0530

    got all asset paths

---
+++ Assets/Scripts/AssetCollector.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AssetCollector : EditorWindow
+{
+    [MenuItem("Window/Asset Collecter")]
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(AssetCollector));
+    }
+
+    Vector2 scroll;
+    List<string> assetPaths = new List<string>();
+    GameObject[] gameObjectsInHeirarchy;
+    List<string> guidsOfGameObjects = new List<string>();
+    List<long> localIdOfGameObjects = new List<long>();
+
+    private void OnGUI()
+    {
+        GUILayout.Label("My string", EditorStyles.boldLabel);
+        scroll = GUILayout.BeginScrollView(scroll);
+        PrintAssets();
+        GetAllGameObjects();
+        PrintGameObjectsName();
+
+        //foreach (GameObject gameObject in gameObjectsInHeirarchy)
+        //{
+        //    if (AssetDatabase.TryGetGUIDAndLocalFileIdentifier(gameObject, out string guid, out long localId))
+        //    {
+        //        Debug.Log(gameObject.name + " " + guid + " " + localId);
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("try function not executed");
+        //    }
+        //}
+        GUILayout.EndScrollView();
+    }
+
+    private void PrintGameObjectsName()
+    {
+        foreach(GameObject gameObject in gameObjectsInHeirarchy)
+        {
+            Debug.Log(gameObject.name + gameObject);
+        }
+    }
+
+    private void PrintAssets()
+    {
+        foreach (string s in AssetDatabase.GetAllAssetPaths())
+        {
+            
+            if (s.StartsWith("Assets/") && !(s.Contains("TextMesh Pro")))
+            {
+                GUILayout.Label(s);
+                assetPaths.Add(s);
+            }
+        }
+    }
+
+    private void GetAllGameObjects()
+    {
+        gameObjectsInHeirarchy = FindObjectsOfType<GameObject>();
+    }
+}
--- Assets/Scripts/ThemeLoaderScript.cs
+++ Assets/Scripts/ThemeLoaderScript.cs
@@ -6,11 +6,10 @@ using UnityEngine.UI;
 public class Theme
 {
     public string name;
-    public List<Color32> list;
+    public Color32[] themeColors = new Color32[3]; 
 }
 
-[CreateAssetMenu(fileName = "Theme Loader", menuName = "ThemeLoader/Themes")]
-public class ThemeLoaderScript : ScriptableObject
+public class ThemeLoaderScript : MonoBehaviour
 {
     [SerializeField]
     private List<Theme> themes;
@@ -30,22 +29,34 @@ public class ThemeLoaderScript : ScriptableObject
         if(currentTheme == themes[0].name)
         {
             Debug.Log("set theme 1");
-            backGround.color = themes[0].list[0];
-            board.color = themes[0].list[1];
+            backGround.color = themes[0].themeColors[0];
+            board.color = themes[0].themeColors[1];
             for (int i = 0; i < 4; i++)
             {
-                gridLines.transform.GetChild(i).gameObject.GetComponent<Image>().color = themes[0].list[2];
+                gridLines.transform.GetChild(i).gameObject.GetComponent<Image>().color = themes[0].themeColors[2];
             }
         }
         else if(currentTheme == themes[1].name)
         {
             Debug.Log("set theme 2");
-            backGround.color = themes[1].list[0];
-            board.color = themes[1].list[1];
+            backGround.color = themes[1].themeColors[0];
+            board.color = themes[1].themeColors[1];
             for (int i = 0; i < 4; i++)
             {
-                gridLines.transform.GetChild(i).gameObject.GetComponent<Image>().color = themes[1].list[2];
+                gridLines.transform.GetChild(i).gameObject.GetComponent<Image>().color = themes[1].themeColors[2];
             }
+        }
+        Reload(false);
+        Reload(true);
+    }
+
+    private void Reload(bool toggle)
+    {
+        backGround.gameObject.SetActive(toggle);
+        board.gameObject.SetActive(toggle);
+        for (int i = 0; i < 4; i++)
+        {
+            gridLines.transform.GetChild(i).gameObject.SetActive(toggle);
         }
     }
 }
